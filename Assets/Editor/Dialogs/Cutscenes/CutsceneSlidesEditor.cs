445552a91991d41ae745d0ebd593085d364c78a3
Victorma Perez Colado <victormp@ucm.es>
2017-07-20 16:18:32 +0200

    Animation transition management in editor

--- Assets/Editor/Dialogs/Cutscenes/CutsceneSlidesEditor.cs
+++ Assets/Editor/Dialogs/Cutscenes/CutsceneSlidesEditor.cs
@@ -18,16 +18,20 @@ namespace uAdventure.Editor
         private Texture2D clearImg = null;
         private Texture2D duplicateImg = null;
 
+        private string[] transitionTypeName;
+        private Texture2D[] transitionTypeTexture;
+
         private static GUISkin defaultSkin;
         private static GUISkin noBackgroundSkin;
         private static GUISkin selectedFrameSkin;
 
-        private bool useTransitonFlag, slidesAnimationFlag, useTransitonFlagLast, slidesAnimationFlagLast;
+        private bool useTransitonFlag, slidesAnimationFlag;
 
-        private string documentationTextContent = "";
+        private string documentationTextContent = "", frameDocumentation = "";
         private string imagePath = "", soundPath = "";
 
         private long animationDuration = 40, transitionDuration = 0;
+        private int transitionType;
 
         private Animation workingAnimation;
 
@@ -50,16 +54,24 @@ namespace uAdventure.Editor
             windowHeight = 1000;
 
             cutscenePath = cutsceneFilePath;
-            clearImg = (Texture2D)Resources.Load("EAdventureData/img/icons/deleteContent", typeof(Texture2D));
-            addTexture = (Texture2D)Resources.Load("EAdventureData/img/icons/addNode", typeof(Texture2D));
-            moveLeft = (Texture2D)Resources.Load("EAdventureData/img/icons/moveNodeLeft", typeof(Texture2D));
-            moveRight = (Texture2D)Resources.Load("EAdventureData/img/icons/moveNodeRight", typeof(Texture2D));
-            duplicateImg = (Texture2D)Resources.Load("EAdventureData/img/icons/duplicateNode", typeof(Texture2D));
+            clearImg = Resources.Load<Texture2D>("EAdventureData/img/icons/deleteContent");
+            addTexture = Resources.Load<Texture2D>("EAdventureData/img/icons/addNode");
+            moveLeft = Resources.Load<Texture2D>("EAdventureData/img/icons/moveNodeLeft");
+            moveRight = Resources.Load<Texture2D>("EAdventureData/img/icons/moveNodeRight");
+            duplicateImg = Resources.Load<Texture2D>("EAdventureData/img/icons/duplicateNode");
 
-            noBackgroundSkin = (GUISkin)Resources.Load("Editor/EditorNoBackgroundSkin", typeof(GUISkin));
-            selectedFrameSkin = (GUISkin)Resources.Load("Editor/EditorLeftMenuItemSkinConcreteOptions", typeof(GUISkin));
+            noBackgroundSkin = Resources.Load<GUISkin>("Editor/EditorNoBackgroundSkin");
+            selectedFrameSkin = Resources.Load<GUISkin>("Editor/EditorLeftMenuItemSkinConcreteOptions");
 
-            //transitionTypes = new string []{ "None" , "Fade in", "Horizontal", "Vertical"};
+
+            transitionTypeName = new string []{ "None" , "Fade in", "Horizontal", "Vertical"};
+            transitionTypeTexture = new Texture2D[]
+            {
+                Resources.Load<Texture2D>("EAdventureData/img/icons/transitionNone"),
+                Resources.Load<Texture2D>("EAdventureData/img/icons/transitionFadein"),
+                Resources.Load<Texture2D>("EAdventureData/img/icons/transitionHorizontal"),
+                Resources.Load<Texture2D>("EAdventureData/img/icons/transitionVertical")
+            };
             Debug.Log(cutsceneFilePath);
 
             workingAnimation = Loader.loadAnimation(AssetsController.InputStreamCreatorEditor.getInputStreamCreator(),
@@ -71,6 +83,10 @@ namespace uAdventure.Editor
 
             // Initalize
             selectedFrame = -1;
+            useTransitonFlag = workingAnimation.isUseTransitions();
+            slidesAnimationFlag = workingAnimation.isSlides();
+
+            documentationTextContent = workingAnimation.getDocumentation();
             OnFrameSelectionChanged(selectedFrame);
 
             base.Init(e);
@@ -79,15 +95,17 @@ namespace uAdventure.Editor
         void OnGUI()
         {
             EditorGUILayout.PrefixLabel(TC.get("Animation.GeneralInfo"), GUIStyle.none, titleStyle);
+            EditorGUI.BeginChangeCheck();
             documentationTextContent = EditorGUILayout.TextField(TC.get("Animation.Documentation"), documentationTextContent);
+            if (EditorGUI.EndChangeCheck()) workingAnimation.setDocumentation(documentationTextContent);
 
+            EditorGUI.BeginChangeCheck();
             useTransitonFlag = EditorGUILayout.Toggle(TC.get("Animation.UseTransitions"), useTransitonFlag);
-            if (useTransitonFlag != useTransitonFlagLast)
-                OnUseTransitonFlagLastChanged(useTransitonFlag);
+            if (EditorGUI.EndChangeCheck()) OnUseTransitonFlagLastChanged(useTransitonFlag);
 
+            EditorGUI.BeginChangeCheck();
             slidesAnimationFlag = EditorGUILayout.Toggle(TC.get("Animation.Slides"), slidesAnimationFlag);
-            if (slidesAnimationFlag != slidesAnimationFlagLast)
-                OnSlidesAnimationFlagLastChanged(slidesAnimationFlag);
+            if(EditorGUI.EndChangeCheck()) OnSlidesAnimationFlagLastChanged(slidesAnimationFlag);
 
             /*
              * Transition panel
@@ -101,11 +119,20 @@ namespace uAdventure.Editor
                 if (selectedFrame == i)
                     GUI.skin = selectedFrameSkin;
 
-                if (
-                    GUILayout.Button(
-                        workingAnimation.getFrames()[i].getImage(false, false, global::uAdventure.Core.Animation.ENGINE).texture, GUILayout.Height(100), GUILayout.Width(80)))
+                var frame = workingAnimation.getFrame(i);
+                var frameContent = new GUIContent(frame.getTime().ToString(), frame.getImage(false, false, global::uAdventure.Core.Animation.ENGINE).texture);
+                if (GUILayout.Button(frameContent, GUILayout.Height(100), GUILayout.Width(80)))
                 {
                     OnFrameSelectionChanged(i);
+                }
+                if (useTransitonFlag && i != workingAnimation.getFrames().Count-1)
+                {
+                    var transition = workingAnimation.getTranstionForFrame(i);
+                    var transitionContent = new GUIContent(transition.getTime().ToString(), transitionTypeTexture[transition.getType()]);
+                    if (GUILayout.Button(transitionContent, GUILayout.Height(100), GUILayout.Width(80)))
+                    {
+                        OnFrameSelectionChanged(i);
+                    }
                 }
                 GUI.skin = noBackgroundSkin;
             }
@@ -165,6 +192,10 @@ namespace uAdventure.Editor
             EditorGUILayout.PrefixLabel(TC.get("Animation.Details"), GUIStyle.none, titleStyle);
 
             EditorGUI.BeginChangeCheck();
+            frameDocumentation = EditorGUILayout.TextField(TC.get("Animation.Documentation"), frameDocumentation);
+            if (EditorGUI.EndChangeCheck()) OnFrameDocumentationChanged(frameDocumentation);
+
+            EditorGUI.BeginChangeCheck();
             animationDuration = EditorGUILayout.LongField(TC.get("Animation.Duration"), animationDuration);
             if (EditorGUI.EndChangeCheck()) OnFrameDurationChanged(animationDuration);
             
@@ -195,13 +226,21 @@ namespace uAdventure.Editor
             }
             EditorGUILayout.EndHorizontal();
 
+            GUI.enabled = useTransitonFlag && selectedFrame != -1 && selectedFrame < workingAnimation.getFrames().Count - 1;
+
+            EditorGUILayout.PrefixLabel(TC.get("NextScene.Transition"), GUIStyle.none, titleStyle);
             EditorGUI.BeginChangeCheck();
-            transitionDuration = EditorGUILayout.LongField(TC.get("NextScene.Transition") + " " + TC.get("Animation.Duration"), transitionDuration);
+            transitionDuration = EditorGUILayout.LongField(TC.get("Animation.Duration"), transitionDuration);
             if (EditorGUI.EndChangeCheck()) OnTransitionDurationChanged(transitionDuration);
+
+            EditorGUI.BeginChangeCheck();
+            transitionType = EditorGUILayout.Popup(TC.get("Conditions.Type"), transitionType, transitionTypeName); // TODO create a type in TC for transition 
+            if (EditorGUI.EndChangeCheck()) OnTransitionTypeChanged(transitionType);
 
             GUILayout.FlexibleSpace();
 
             EditorGUILayout.BeginHorizontal();
+            GUI.enabled = true;
             if (GUILayout.Button("OK"))
             {
                 AnimationWriter.writeAnimation(cutscenePath, workingAnimation);
@@ -222,44 +261,50 @@ namespace uAdventure.Editor
 
             if (selectedFrame != -1)
             {
-                imagePath = workingAnimation.getFrames()[selectedFrame].getUri();
-                soundPath = workingAnimation.getFrames()[selectedFrame].getSoundUri();
-                
-                documentationTextContent = workingAnimation.getFrames()[selectedFrame].getDocumentation();
+                var frame = workingAnimation.getFrame(selectedFrame);
+                imagePath = frame.getUri();
+                soundPath = frame.getSoundUri();
 
-                animationDuration = workingAnimation.getFrames()[selectedFrame].getTime();
-                transitionDuration = workingAnimation.getTransitions()[selectedFrame + 1].getTime();
+                frameDocumentation = frame.getDocumentation();
+
+                animationDuration = frame.getTime();
+                transitionDuration = workingAnimation.getTranstionForFrame(selectedFrame).getTime();
+                transitionType = workingAnimation.getTranstionForFrame(selectedFrame).getType();
             }
             else
             {
-                imagePath = soundPath = documentationTextContent = string.Empty;
-                animationDuration = transitionDuration = 0;
+                imagePath = soundPath = frameDocumentation = string.Empty;
+                animationDuration = transitionDuration = 0; 
             }
 
         }
 
         private void OnSlidesAnimationFlagLastChanged(bool val)
         {
-            slidesAnimationFlagLast = val;
             workingAnimation.setSlides(val);
         }
 
         private void OnUseTransitonFlagLastChanged(bool val)
         {
-            useTransitonFlagLast = val;
             workingAnimation.setUseTransitions(val);
         }
 
         private void OnFrameDurationChanged(long dur)
         {
-            workingAnimation.getFrame(selectedFrame).setTime(dur);
+            animationDuration = System.Math.Max(0, dur);
+            workingAnimation.getFrame(selectedFrame).setTime(animationDuration);
         }
 
         private void OnTransitionDurationChanged(long dur)
         {
-            workingAnimation.getTransitions()[selectedFrame + 1].setTime(dur);
+            transitionDuration = System.Math.Max(0, dur);
+            workingAnimation.getTranstionForFrame(selectedFrame).setTime(transitionDuration); 
         }
-
+        
+        private void OnTransitionTypeChanged(int type)
+        {
+            workingAnimation.getTranstionForFrame(selectedFrame).setType(type);
+        }
 
         private void OnFrameImageChanged(string val)
         {
@@ -271,6 +316,10 @@ namespace uAdventure.Editor
         {
             soundPath = val;
             workingAnimation.getFrame(selectedFrame).setSoundUri(val);
+        }
+        private void OnFrameDocumentationChanged(string val)
+        {
+            workingAnimation.getFrame(selectedFrame).setDocumentation(val);
         }
 
         public void OnDialogOk(string message, object workingObject = null, object workingObjectSecond = null)
