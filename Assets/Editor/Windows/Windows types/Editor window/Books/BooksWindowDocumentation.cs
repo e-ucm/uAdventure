bf43e2af7584c5b14455f9c1af42ee5be8a2775e
Victorma Perez Colado <victormp@ucm.es>
2017-11-06 18:00:26 +0100

    Remake of books documentation

--- Assets/Editor/Windows/Windows types/Editor window/Books/BooksWindowDocumentation.cs
+++ Assets/Editor/Windows/Windows types/Editor window/Books/BooksWindowDocumentation.cs
@@ -1,40 +1,29 @@
 ﻿using UnityEngine;
 
 using uAdventure.Core;
+using UnityEditor;
 
 namespace uAdventure.Editor
 {
     public class BooksWindowDocumentation : LayoutWindow
     {
-        private string documentation, documentationLast;
-
         public BooksWindowDocumentation(Rect aStartPos, GUIContent aContent, GUIStyle aStyle,
             params GUILayoutOption[] aOptions)
             : base(aStartPos, aContent, aStyle, aOptions)
         {
-            string doc = "";
-
-            if (GameRources.GetInstance().selectedCutsceneIndex >= 0)
-                doc = Controller.Instance.ChapterList.getSelectedChapterData().getBooks()[
-                    GameRources.GetInstance().selectedBookIndex].getDocumentation();
-            doc = (doc == null ? "" : doc);
-            documentation = documentationLast = doc;
         }
 
         public override void Draw(int aID)
         {
+            var workingBook = Controller.Instance.ChapterList.getSelectedChapterData().getBooks()[GameRources.GetInstance().selectedBookIndex];
+
             GUILayout.Space(20);
             GUILayout.Label(TC.get("Book.Documentation"));
             GUILayout.Space(20);
-            documentation = GUILayout.TextArea(documentation, GUILayout.MinHeight(0.4f * m_Rect.height));
-            if (!documentation.Equals(documentationLast))
-                OnDocumentationChanged(documentation);
-        }
-
-        private void OnDocumentationChanged(string s)
-        {
-            Controller.Instance.ChapterList.getSelectedChapterData().getBooks()[GameRources.GetInstance().selectedBookIndex].setDocumentation(s);
-            documentationLast = s;
+            EditorGUI.BeginChangeCheck();
+            var documentation = GUILayout.TextArea(workingBook.getDocumentation(), GUILayout.MinHeight(0.4f * m_Rect.height));
+            if (EditorGUI.EndChangeCheck())
+                workingBook.setDocumentation(documentation);
         }
     }
 }
