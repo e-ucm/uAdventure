1fb0e1e00542106176aa83de7ae547e5ce86453b
Victorma <victorma-fire@hotmail.com>
2019-10-29 06:16:47 +0100

    Fixed missing parethesis on merge

--- Assets/uAdventure/Editor/Windows/EditorWindowBase.cs
+++ Assets/uAdventure/Editor/Windows/EditorWindowBase.cs
@@ -176,6 +176,7 @@ namespace uAdventure.Editor
             if (debugGUIHolder)
             {
                 DestroyImmediate(debugGUIHolder);
+            }
         }
 
         public static void RefreshLanguage(){
