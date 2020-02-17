4b572991428709b61fed1abf9ac6cb6d0adc7f9b
Synpheros <Synpheros@hotmail.com>
2020-02-17 20:32:38 +0100

    Added monobehaviour to close the game

---
+++ Assets/uAdventureAnalytics/Plugins/Simva/ApplicationQuitter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuitter : MonoBehaviour {
+
+    public static void Quit()
+    {
+        Application.Quit();
+    }
+}
