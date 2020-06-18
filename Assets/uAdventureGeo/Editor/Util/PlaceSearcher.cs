3e7db7ff15492cc38d611cdc66e99111ded6491b
Victorma <victorma-fire@hotmail.com>
2020-06-18 03:07:18 +0200

    Updated place searcher to use json

--- Assets/uAdventureGeo/Editor/Util/PlaceSearcher.cs
+++ Assets/uAdventureGeo/Editor/Util/PlaceSearcher.cs
@@ -6,9 +6,87 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.Networking;
 using uAdventure.Editor;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace uAdventure.Geo
 {
+    [DataContract]
+    public class Place
+    {
+        [DataMember(Name = "place_id", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "place_id")]
+        public long PlaceId { get; set; }
+
+        [DataMember(Name = "licence", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "licence")]
+        public string Licence { get; set; }
+
+        [DataMember(Name = "osm_type", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "extraosm_type_data")]
+        public string OsmType { get; set; }
+
+        [DataMember(Name = "osm_id", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "osm_id")]
+        public long OsmId { get; set; }
+
+        [DataMember(Name = "boundingbox", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "boundingbox")]
+        public double[] BoundingBox { get; set; }
+
+        [DataMember(Name = "lat", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "lat")]
+        public double Lat { get; set; }
+
+        [DataMember(Name = "lon", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "lon")]
+        public double Lon { get; set; }
+
+        [DataMember(Name = "display_name", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "display_name")]
+        public string DisplayName { get; set; }
+
+        [DataMember(Name = "class", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "class")]
+        public string Class { get; set; }
+
+        [DataMember(Name = "type", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "importance", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "importance")]
+        public float Importance { get; set; }
+
+        [DataMember(Name = "icon", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "icon")]
+        public string Icon { get; set; }
+
+        [JsonIgnore]
+        public Vector2d LatLon 
+        { 
+            get
+            {
+                return new Vector2d(Lat, Lon);
+            }
+        }
+
+        [JsonIgnore]
+        public RectD RectBoundingBox 
+        { 
+            get
+            {
+                var cornerA = new Vector2d(BoundingBox[0], BoundingBox[2]);
+                var cornerB = new Vector2d(BoundingBox[1], BoundingBox[3]);
+                Debug.Log(cornerA);
+                Debug.Log(cornerB);
+                Debug.Log(cornerB - cornerA);
+                return new RectD(cornerA, cornerB - cornerA);
+            }
+        }
+
+    }
+
     public class PlaceSearcher : ScriptableObject
     {
         const string PATH_SAVE_SCRIPTABLE_OBJECT = "Assets/GeoExpansion/MapzenGo/Resources/Settings/";
@@ -29,8 +107,8 @@ namespace uAdventure.Geo
             get { return addressDropdown.Label; }
             set { addressDropdown.Label = value; }
         }
-        public Vector2d LatLon { get; set; }
-        public RectD BoundingBox { get; set; }
+
+        public Place Place { get; set; }
 
         /* ---------------------------------
          * Attributes
@@ -47,7 +125,7 @@ namespace uAdventure.Geo
         private bool searched;
         private EditorApplication.CallbackFunction update;
         private UnityWebRequestAsyncOperation request;
-        private SearchData[] addresses;
+        private Place[] addresses;
 
         public int Source { get; set; }
 
@@ -97,9 +175,7 @@ namespace uAdventure.Geo
             if (selected)
             {
                 lastSearch = Value;
-                var searchData = addresses.First(sd => sd.label == Value);
-                this.LatLon = searchData.coordinates.ToVector2d();
-                this.BoundingBox = searchData.boundingBox;
+                Place = addresses.First(sd => sd.DisplayName == Value);
             }
 
             if (!selected && Value != prevAddress)
@@ -152,7 +228,7 @@ namespace uAdventure.Geo
                 } 
                 else
                 {
-                    addressDropdown.Elements = addresses.Select(a => a.label).ToList();
+                    addressDropdown.Elements = addresses.Select(a => a.DisplayName).ToList();
                     // Request the repaint of the element 
                     if (OnRequestRepaint != null)
                     {
@@ -171,32 +247,9 @@ namespace uAdventure.Geo
             request = www.SendWebRequest();
         }
 
-        private SearchData[] DataProcessingOSM(string success)
+        private Place[] DataProcessingOSM(string success)
         {
-            JSONObject obj = new JSONObject(success);
-            var dataCache = new List<SearchData>();
-
-            if(obj == null || obj.list == null)
-            {
-                return null;
-            }
-
-            foreach (JSONObject jsonObject in obj.list)
-            {
-                var minLat = double.Parse(jsonObject["boundingbox"][0].str);
-                var maxLat = double.Parse(jsonObject["boundingbox"][1].str);
-                var minLon = double.Parse(jsonObject["boundingbox"][2].str);
-                var maxLon = double.Parse(jsonObject["boundingbox"][3].str);
-
-                dataCache.Add(new SearchData()
-                {
-                    coordinates = new Vector2(float.Parse(jsonObject["lat"].str), float.Parse(jsonObject["lon"].str)),
-                    label = jsonObject["display_name"].str,
-                    boundingBox = new RectD(new Vector2d(minLon, minLat), new Vector2d(maxLon - minLon, maxLat - minLat))
-                });
-            }
-
-            return dataCache.ToArray();
+            return JsonConvert.DeserializeObject<Place[]>(success);
         }
     }
 
