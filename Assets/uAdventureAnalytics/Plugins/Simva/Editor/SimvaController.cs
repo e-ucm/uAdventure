038895c5d1c1b458282525494bf589ff110964a8
Victorma Perez Colado <victorma-fire@hotmail.com>
2021-01-05 06:36:58 +0100

    Fixed Wix installer default path and added shortcuts

--- Assets/uAdventureAnalytics/Plugins/Simva/Editor/SimvaController.cs
+++ Assets/uAdventureAnalytics/Plugins/Simva/Editor/SimvaController.cs
@@ -12,6 +12,7 @@ using UnityEngine.WSA;
 using System.Xml;
 using System.Xml.Linq;
 using uAdventure.Editor;
+using System.Collections.Generic;
 
 namespace uAdventure.Simva
 {
@@ -109,25 +110,6 @@ namespace uAdventure.Simva
             }
         }
 
-        private static Dir GetDirContents(string prepend, string localPath, string folder, bool prependIsBase = true)
-        {
-            var localFolder = prependIsBase ? localPath + folder : localPath + prepend + folder;
-
-            var dir = new Dir(prependIsBase ? prepend + folder : folder);
-
-            foreach (var file in Directory.GetFiles(localFolder))
-            {
-                dir.AddFile(new WixSharp.File(file));
-            }
-
-            foreach(var directory in Directory.GetDirectories(localFolder).Select(d => new DirectoryInfo(d)))
-            {
-                dir.AddDir(GetDirContents(prependIsBase ? folder : prepend + "\\" + folder + "\\", localPath, directory.Name, false));
-            }
-
-            return dir;
-        }
-
         [PostProcessBuild(0)]
         public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
         {
@@ -213,16 +195,36 @@ namespace uAdventure.Simva
         {
             EditorUtility.DisplayProgressBar("Creating Installers", "Preparing Wix project...", 0f);
             Compiler.WixLocation = Directory.GetCurrentDirectory() + "\\wixsharp\\Wix_bin\\bin";
+
             var path = new FileInfo(pathToBuiltProject).Directory.FullName;
-
             var fullSetup = new Feature(PlayerSettings.productName + " Binaries");
+            var productFolder = PlayerSettings.companyName + @"\" + PlayerSettings.productName;
 
+            // Create the project contents
+            var projectContents = GetDirContents(path).ToList();
+
+            // Add the unninstall shortcut
+            projectContents.Add(new ExeFileShortcut("Uninstall " + PlayerSettings.productName, "[System64Folder]msiexec.exe", "/x [ProductCode]") { WorkingDirectory = "[INSTALLDIR]" });
+
+            // Create the project structure
             var project = new Project(PlayerSettings.productName,
-                GetDirContents(@"%ProgramFiles%\" + PlayerSettings.companyName + @"\" + PlayerSettings.productName, path, "\\"))
+
+                // Program files folder
+                new Dir(@"%ProgramFiles%\" + productFolder, projectContents.ToArray()),
+
+                // Program menu folder
+                new Dir(@"%ProgramMenu%\" + productFolder,
+                    new ExeFileShortcut("Uninstall " + PlayerSettings.productName , "[System64Folder]msiexec.exe", "/x [ProductCode]") { WorkingDirectory = "[INSTALLDIR]" },
+                    new ExeFileShortcut(PlayerSettings.productName, "[INSTALLDIR]" + PlayerSettings.productName + ".exe", arguments: "") { WorkingDirectory = "[INSTALLDIR]" }),
+
+                // Desktop (main shortcut)
+                new Dir(@"%Desktop%",
+                    new ExeFileShortcut(PlayerSettings.productName, "[INSTALLDIR]" + PlayerSettings.productName + ".exe", arguments: "") { WorkingDirectory = "[INSTALLDIR]" }))
             {
                 GUID = new System.Guid(PlayerSettings.productGUID.ToString())
             };
 
+            // Add the registry keys for opening the game through URI Protocol
             project.AddRegKey(new RegKey(fullSetup, RegistryHive.CurrentUser, @"Software\Classes\" + PlayerSettings.applicationIdentifier,
                 new RegValue("", "URL " + PlayerSettings.productName + " Link"),
                 new RegValue("URL Protocol", PlayerSettings.productName + " Protocol")));
@@ -238,9 +240,30 @@ namespace uAdventure.Simva
 
             project.OutDir = "Installers/";
             project.OutFileName = PlayerSettings.productName;
+            project.UI = WUI.WixUI_InstallDir; 
+            project.Platform = Platform.x64;
             EditorUtility.DisplayProgressBar("Creating Installers", "Building Msi installer...", 0.1f);
             project.BuildMsi();
             EditorUtility.DisplayProgressBar("Creating Installers", "Done!", 1f);
+        }
+
+        private static WixEntity[] GetDirContents(string installPath, string folder = "")
+        {
+            var folderPath = installPath + @"\" + folder;
+
+            List<WixEntity> contents = new List<WixEntity>();
+
+            foreach (var file in Directory.GetFiles(folderPath))
+            {
+                contents.Add(new WixSharp.File(file));
+            }
+
+            foreach (var directory in Directory.GetDirectories(folderPath).Select(d => new DirectoryInfo(d)))
+            {
+                contents.Add(new Dir(directory.Name, GetDirContents(installPath, folder + directory.Name + @"\")));
+            }
+
+            return contents.ToArray();
         }
     }
 }
