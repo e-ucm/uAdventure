a816bb8004c95a28b0eb66e0502d1e26490d98d7
Victorma Perez Colado <victorma-fire@hotmail.com>
2021-10-11 13:58:49 +0200

    [New] Added character behavior control

--- Assets/uAdventure/Editor/Windows/Windows types/Editor window/Characters/CharactersWindowDocumentation.cs
+++ Assets/uAdventure/Editor/Windows/Windows types/Editor window/Characters/CharactersWindowDocumentation.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 using uAdventure.Core;
 using UnityEditor;
+using System.Linq;
 
 namespace uAdventure.Editor
 {
     [EditorComponent(typeof(NPCDataControl), Name = "NPC.Documentation", Order = 20)]
     public class CharactersWindowDocumentation : AbstractEditorComponent
     {
+        private readonly string[] behaviourTypes = { "Behaviour.Normal", "Behaviour.FirstAction" };
+        private readonly string[] behaviourTypesDescription = { "Behaviour.Selection.Normal", "Behaviour.Selection.FirstAction" };
 
         private NPCDataControl workingCharacter;
         private DescriptionsEditor descriptionsEditor;
@@ -25,23 +28,33 @@ namespace uAdventure.Editor
         {
             workingCharacter = Target != null ? Target as NPCDataControl : Controller.Instance.SelectedChapterDataControl.getNPCsList().getNPCs()[GameRources.GetInstance().selectedCharacterIndex];
 
-
             // -------------
             // Documentation
             // -------------
-
             GUILayout.Label(TC.get("NPC.Documentation"));
             EditorGUI.BeginChangeCheck();
             var fullItemDescription = GUILayout.TextArea(workingCharacter.getDocumentation() ?? string.Empty);
             if (EditorGUI.EndChangeCheck())
                 workingCharacter.setDocumentation(fullItemDescription);
 
-
             // -------------
             // Descriptions
             // -------------
             descriptionsEditor.Descriptions = workingCharacter.getDescriptionController();
             descriptionsEditor.OnInspectorGUI();
+            GUILayout.Space(20);
+
+            // -------------
+            // Behaviour
+            // -------------
+            EditorGUI.BeginChangeCheck();
+            var selectedBehaviourType = EditorGUILayout.Popup(TC.get("Behaviour"), (int)workingCharacter.getBehaviour(), behaviourTypes.Select(bt => TC.get(bt)).ToArray());
+            Item.BehaviourType type = (selectedBehaviourType == 0 ? Item.BehaviourType.NORMAL : Item.BehaviourType.FIRST_ACTION);
+            if (EditorGUI.EndChangeCheck())
+            {
+                workingCharacter.setBehaviour(type);
+            }
+            EditorGUILayout.HelpBox(TC.get(behaviourTypesDescription[selectedBehaviourType]), MessageType.Info);
             GUILayout.Space(20);
         }
     }
